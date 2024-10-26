585fc0b95ad861054873515f84b64bb7df760ce0
Nicolas Beaudouin <pro@nicolasbeaudouin.com>
2024-10-26 20:14:59 +0100

    WIP

--- contracts/crowdfunding/src/lib.rs
+++ contracts/crowdfunding/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-use soroban_sdk::{contract, contracterror, contractimpl, contracttype, vec, Env, String, Vec, Timepoint};
+use soroban_sdk::{contract, contracterror, contractimpl, contracttype, vec, Address, BytesN, Env, String, Timepoint, Vec};
 
 #[contract]
 pub struct CrowdfundingContract;
@@ -7,9 +7,12 @@ pub struct CrowdfundingContract;
 #[contracttype]
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct CrowdfundingState {
+    pub owner : Address,
     pub balance: u32,
-    pub target: u32,
-    // pub duration: Timepoint,
+    pub target_price: u32,
+    pub name: String,
+    pub deadline: Timepoint,
+    pub project_description: BytesN<32>,
 }
 
 #[contracterror]
@@ -25,34 +28,32 @@ impl CrowdfundingContract {
         vec![&env, String::from_str(&env, "Hello"), to]
     }
 
-    pub fn init_crowfunding(env: Env, crowdfund_id: u32, target: u32) {
+    pub fn init_crowfunding(env: Env, crowdfund_id: u32, target: u32, name: String, deadline: Timepoint, project_description: BytesN<32>) {
         let state = CrowdfundingState {
             balance: 0,
-            target: target,
-            time: env.time(),
+            target_price: target,
+            name,
+            deadline,
+            project_description,
         };
         env.storage().persistent().set(&crowdfund_id, &state);
     }
 
     pub fn give(env: Env, amount: u32, crowdfund_id: u32) -> u32{
-        let current_state : CrowdfundingState = env.storage().persistent().get(&crowdfund_id).unwrap();
-        let new_state = CrowdfundingState {
-            balance: current_state.balance + amount,
-            target: current_state.target,
-        };
-        env.storage().persistent().set(&crowdfund_id, &(new_state));
-        return new_state.balance;
+        let mut current_state : CrowdfundingState = env.storage().persistent().get(&crowdfund_id).unwrap();
+        current_state.balance += amount;
+        env.storage().persistent().set(&crowdfund_id, &(current_state));
+        return current_state.balance;
     }
 
     pub fn withdraw(env: Env, crowdfund_id: u32) -> Result<(), Error> {
-        let current_state : CrowdfundingState = env.storage().persistent().get(&crowdfund_id).unwrap();
-        if !current_state.balance >= current_state.target {
+        let mut current_state : CrowdfundingState = env.storage().persistent().get(&crowdfund_id).unwrap();
+        if current_state.balance < current_state.target_price {
             Err(Error::TargetNotAchieved)
         } else {
-            env.storage().persistent().set(&crowdfund_id, &CrowdfundingState {
-                balance: 0,
-                target: 0,
-            });
+            current_state.balance = 0;
+            current_state.target_price = 0;
+            env.storage().persistent().set(&crowdfund_id, &current_state);
             // Send the money to the project owner
             Ok(())
         }
--- contracts/crowdfunding/src/test.rs
+++ contracts/crowdfunding/src/test.rs
@@ -4,7 +4,20 @@ use super::*;
 use soroban_sdk::{vec, Env, String};
 
 #[test]
-fn test() {
+fn test_hello_world() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, CrowdfundingContract);
+    let client = CrowdfundingContractClient::new(&env, &contract_id);
+
+    let result = client.give(&1,&2);
+    assert_eq!(
+        result,
+        1
+    );
+}
+
+#[test]
+fn test_add_funds() {
     let env = Env::default();
     let contract_id = env.register_contract(None, CrowdfundingContract);
     let client = CrowdfundingContractClient::new(&env, &contract_id);
