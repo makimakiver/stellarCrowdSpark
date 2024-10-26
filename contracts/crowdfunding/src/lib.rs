8d0517400b5dcfeff84ebf37d4b8f948e6564896
Nicolas Beaudouin <pro@nicolasbeaudouin.com>
2024-10-26 20:14:59 +0100

    [UPD] Contract improvement

--- contracts/crowdfunding/src/lib.rs
+++ contracts/crowdfunding/src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
-use soroban_sdk::{contract, contracterror, contractimpl, contracttype, vec, Env, String, Vec, Timepoint};
+use soroban_sdk::{
+    contract, contracterror, contractimpl, contracttype, token, Address, Env, Map, String,
+};
 
 #[contract]
 pub struct CrowdfundingContract;
@@ -7,54 +9,120 @@ pub struct CrowdfundingContract;
 #[contracttype]
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct CrowdfundingState {
-    pub balance: u32,
-    pub target: u32,
-    // pub duration: Timepoint,
+    pub owner: Address,
+    pub balance: i128,
+    pub target_price: i128,
+    pub name: String,
+    pub deadline: u64,
+    pub token: Address,
+    pub contributions: Map<Address, i128>,
 }
 
 #[contracterror]
 #[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
 #[repr(u32)]
 pub enum Error {
-    TargetNotAchieved = 1,
+    InvalidDeadline = 1,
+    CrowfundingAlreadyClosed = 2,
+    CrowfundingStillOpen = 3,
 }
 
 #[contractimpl]
 impl CrowdfundingContract {
-    pub fn hello(env: Env, to: String) -> Vec<String> {
-        vec![&env, String::from_str(&env, "Hello"), to]
-    }
+    pub fn create_crowdfunding(
+        env: Env,
+        token: Address,
+        user: Address,
+        crowdfund_id: u32,
+        target_price: i128,
+        name: String,
+        deadline: u64,
+    ) -> Result<(), Error> {
+        // Only owner can create a crowdfunding: this also ensures less possibility of loss of funds
+        user.require_auth();
 
-    pub fn init_crowfunding(env: Env, crowdfund_id: u32, target: u32) {
+        let ledger_timestamp = env.ledger().timestamp();
+        if deadline < ledger_timestamp {
+            return Err(Error::InvalidDeadline);
+        }
         let state = CrowdfundingState {
+            owner: user,
             balance: 0,
-            target: target,
-            time: env.time(),
+            target_price,
+            name,
+            deadline,
+            token,
+            contributions: Map::new(&env),
         };
+
+        // Store the state
         env.storage().persistent().set(&crowdfund_id, &state);
+        Ok(())
     }
 
-    pub fn give(env: Env, amount: u32, crowdfund_id: u32) -> u32{
-        let current_state : CrowdfundingState = env.storage().persistent().get(&crowdfund_id).unwrap();
-        let new_state = CrowdfundingState {
-            balance: current_state.balance + amount,
-            target: current_state.target,
-        };
-        env.storage().persistent().set(&crowdfund_id, &(new_state));
-        return new_state.balance;
-    }
+    pub fn donate(
+        env: Env,
+        sender: Address,
+        amount: i128,
+        crowdfund_id: u32,
+    ) -> Result<i128, Error> {
+        // We need to ensure the sender is authenticated
+        sender.require_auth();
+        let mut crowdfunding: CrowdfundingState =
+            env.storage().persistent().get(&crowdfund_id).unwrap();
+        let ledger_timestamp = env.ledger().timestamp();
+        if ledger_timestamp > crowdfunding.deadline {
+            return Err(Error::CrowfundingAlreadyClosed);
+        }
 
-    pub fn withdraw(env: Env, crowdfund_id: u32) -> Result<(), Error> {
-        let current_state : CrowdfundingState = env.storage().persistent().get(&crowdfund_id).unwrap();
-        if !current_state.balance >= current_state.target {
-            Err(Error::TargetNotAchieved)
+        // Transfer the funds to the contract
+        token::Client::new(&env, &crowdfunding.token).transfer(
+            &sender,
+            &env.current_contract_address(),
+            &amount,
+        );
+
+        // Update the balance and contributions
+        crowdfunding.balance += amount;
+        if let Some(prev_value) = crowdfunding.contributions.get(sender.clone()) {
+            crowdfunding.contributions.set(sender, prev_value + amount);
         } else {
-            env.storage().persistent().set(&crowdfund_id, &CrowdfundingState {
-                balance: 0,
-                target: 0,
-            });
+            crowdfunding.contributions.set(sender, amount);
+        }
+        env.storage()
+            .persistent()
+            .set(&crowdfund_id, &(crowdfunding));
+        Ok(crowdfunding.balance)
+    }
+
+    pub fn withdraw_funds(env: Env, crowdfund_id: u32) -> Result<(), Error> {
+        let mut crowdfunding: CrowdfundingState =
+            env.storage().persistent().get(&crowdfund_id).unwrap();
+        let ledger_timestamp = env.ledger().timestamp();
+        if ledger_timestamp < crowdfunding.deadline {
+            return Err(Error::CrowfundingStillOpen);
+        } else if crowdfunding.balance < crowdfunding.target_price {
+            // Send the money back to the contributors
+            for (contributor, amount) in crowdfunding.contributions.iter() {
+                token::Client::new(&env, &crowdfunding.token).transfer(
+                    &env.current_contract_address(),
+                    &contributor,
+                    &amount,
+                );
+            }
+            crowdfunding.balance = 0;
+            env.storage().persistent().set(&crowdfund_id, &crowdfunding);
+            return Ok(());
+        } else {
             // Send the money to the project owner
-            Ok(())
+            token::Client::new(&env, &crowdfunding.token).transfer(
+                &env.current_contract_address(),
+                &crowdfunding.owner,
+                &crowdfunding.balance,
+            );
+            crowdfunding.balance = 0;
+            env.storage().persistent().set(&crowdfund_id, &crowdfunding);
+            return Ok(());
         }
     }
 }
--- contracts/crowdfunding/src/test.rs
+++ contracts/crowdfunding/src/test.rs
@@ -1,17 +1,39 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{vec, Env, String};
+use soroban_sdk::{Address, Env};
+use token::{StellarAssetClient, TokenClient};
+use soroban_sdk::testutils::{Address as _, Ledger};
+
+
+fn create_token_contract<'a>(e: &Env, admin: &Address) -> (TokenClient<'a>, StellarAssetClient<'a>) {
+    let sac = e.register_stellar_asset_contract_v2(admin.clone());
+    (
+        token::Client::new(e, &sac.address()),
+        token::StellarAssetClient::new(e, &sac.address()),
+    )
+}
 
 #[test]
-fn test() {
+fn test_init() {
     let env = Env::default();
+    env.mock_all_auths();
+    let current_time = 12345;
+    env.ledger().with_mut(|li| {
+        li.timestamp = current_time;
+    });
+    let user = Address::generate(&env);
+    // let token_address = native_asset_contract_address(&env);
+    let (_token_client,sac_client) = create_token_contract(&env,&user);
+    let token_address = sac_client.address;  
+
     let contract_id = env.register_contract(None, CrowdfundingContract);
     let client = CrowdfundingContractClient::new(&env, &contract_id);
-
-    let result = client.give(&1,&2);
+    let crowd_id = 1;
+    let name = String::from_str(&env,"test");
+    let result = client.create_crowdfunding(&token_address,&user,&crowd_id,&100, &name, &current_time);
     assert_eq!(
         result,
-        1
+        ()
     );
 }
