dc4eb0a14687844a29d482b1e7a8b82a9891abdd
Nicolas Beaudouin <pro@nicolasbeaudouin.com>
2024-10-27 05:11:24 +0000

    [UPD] dependencies for using wallet

---
+++ src/contracts/util.ts
@@ -0,0 +1,3 @@
+export const rpcUrl = import.meta.env.PUBLIC_SOROBAN_RPC_URL ?? "https://soroban-testnet.stellar.org:443"
+export const networkPassphrase = import.meta.env.PUBLIC_SOROBAN_NETWORK_PASSPHRASE ?? "Test SDF Network ; September 2015"
+export const sorobanNetwork= import.meta.env.SOROBAN_NETWORK ?? "testnet"
