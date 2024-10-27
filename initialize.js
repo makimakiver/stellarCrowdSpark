83d893a87837d412c1987acbf4f4bfb56e1b8044
Nicolas Beaudouin <pro@nicolasbeaudouin.com>
2024-10-27 05:05:26 +0000

    [UPD] the initialize file

--- initialize.js
+++ initialize.js
@@ -29,7 +29,7 @@ function exe(command) {
 }
 
 function fundAll() {
-  exe(`${cli} keys generate ${process.env.SOROBAN_ACCOUNT}`);
+  exe(`${cli} keys generate ${args}  ${account}`);
 }
 
 function removeFiles(pattern) {
@@ -55,21 +55,20 @@ function deployAll() {
   const contractsDir = `${dirname}/.soroban/contract-ids`;
   mkdirSync(contractsDir, { recursive: true });
 
-  const wasmFiles = glob(`${dirname}/target/wasm32-unknown-unknown/release/*.wasm`);
+  const wasmFiles = glob(`${dirname}/target/wasm32-unknown-unknown/release/*.wasm ${args} --source-account=${account}`);
 
   wasmFiles.forEach(deploy);
 }
 
 function contracts() {
   const contractFiles = glob(`${dirname}/.soroban/contract-ids/*.json`);
-
   return contractFiles
     .map(path => ({
       alias: filenameNoExtension(path),
       ...JSON.parse(readFileSync(path))
     }))
-    .filter(data => data.ids[process.env.SOROBAN_NETWORK_PASSPHRASE])
-    .map(data => ({alias: data.alias, id: data.ids[process.env.SOROBAN_NETWORK_PASSPHRASE]}));
+    .filter(data => data.ids[passphrase])
+    .map(data => ({alias: data.alias, id: data.ids[passphrase]}));
 }
 
 function bind({alias, id}) {
@@ -84,15 +83,14 @@ function importContract({id, alias}) {
   const outputDir = `${dirname}/src/contracts/`;
 
   mkdirSync(outputDir, { recursive: true });
-
   const importContent =
     `import * as Client from '${alias}';\n` +
     `import { rpcUrl } from './util';\n\n` +
     `export default new Client.Client({\n` +
-    `  ...Client.networks.${process.env.SOROBAN_NETWORK},\n` +
+    `  ...Client.networks.${sorobanNetwork},\n` +
     `  rpcUrl,\n` +
     `${
-      process.env.SOROBAN_NETWORK === "local" || "standalone"
+      sorobanNetwork === "local" || "standalone"
         ? `  allowHttp: true,\n`
         : null
     }` +
@@ -109,6 +107,10 @@ function importAll() {
   contracts().forEach(importContract);
 }
 
+const account = process.env.SOROBAN_ACCOUNT ?? "default";
+const passphrase = process.env.SOROBAN_NETWORK_PASSPHRASE ?? "Test SDF Network ; September 2015";
+const sorobanNetwork = process.env.SOROBAN_NETWORK ?? "testnet";
+const args = `--rpc-url=${process.env.SOROBAN_RPC_URL ?? "https://soroban-testnet.stellar.org:443"} --network-passphrase="${passphrase}" --network="${sorobanNetwork}"`;
 // Calling the functions (equivalent to the last part of your bash script)
 fundAll();
 buildAll();
